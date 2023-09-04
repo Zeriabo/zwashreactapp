639bfff9ea5eb0a13f1c05d78dbb5a2afc245093
Zeriabo <zeriab@hotmail.com>
2023-09-04 15:24:22 +0300

    dashboard and getting stations

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,47 @@
 import React, { useEffect } from "react";
-import { selectUser } from "../slices/userSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "../slices/userSlice";
+import { fetchStations, deleteStation } from "../slices/stationsSlice";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import Button from "@mui/material/Button"; // Import MUI Button
+
 const Dashboard = () => {
-  const dispatch = useDispatch;
-  useEffect(() => {});
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
-  console.log(user);
+  const stations = useSelector((state) => state.station.stations);
+
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchStations(user.id));
+    }
+  }, [dispatch, user]);
+
+  const handleDeleteStation = (stationId) => {
+    dispatch(deleteStation(stationId));
+  };
+
   return (
     <div>
       <h2>Welcome, {user ? user.username : "Guest"}!</h2>
-      {/* You can add more dashboard content here */}
+
+      {/* Link to the "Create Station" page */}
+      <Link to="/create-station">
+        <Button variant="contained" color="primary">
+          Create Station
+        </Button>
+      </Link>
+
+      <h3>List of Stations:</h3>
+      <ul>
+        {stations.map((station) => (
+          <li key={station.id}>
+            {station.name}
+            <button onClick={() => handleDeleteStation(station.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 // reducers/index.js
 import { combineReducers } from "redux";
 import userReducer from "../slices/userSlice";
+import stationReducer from "../slices/stationsSlice";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  station: stationReducer,
 });
 
 export default rootReducer;
